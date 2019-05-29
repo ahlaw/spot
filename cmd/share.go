cd829a1370b6b2d562d66b88eaa3e45f9add995a
Amos Law <amos.law98@gmail.com>
2019-05-29 10:44:11 -0400

    Default share command to copy current context instead of track

--- cmd/share.go
+++ cmd/share.go
@@ -9,20 +9,33 @@ import (
 
 func init() {
 	rootCmd.AddCommand(shareCmd)
+	shareCmd.Flags().BoolP("track", "t", false, "Copy current track instead of context")
 }
 
 var shareCmd = &cobra.Command{
 	Use:   "share",
-	Short: "Copy Open Spotify URL of current track",
+	Short: "Copy Open Spotify URL of current context",
 	RunE:  share,
 }
 
 func share(cmd *cobra.Command, args []string) error {
+	trackFlag, err := cmd.Flags().GetBool("track")
+	if err != nil {
+		return err
+	}
+
 	currPlayerStatus, err := client.PlayerCurrentlyPlaying()
 	if err != nil {
 		return err
 	}
-	url := currPlayerStatus.Item.SimpleTrack.ExternalURLs["spotify"]
+
+	var url string
+	contextType := currPlayerStatus.PlaybackContext.Type
+	if trackFlag || contextType == "" {
+		url = currPlayerStatus.Item.SimpleTrack.ExternalURLs["spotify"]
+	} else {
+		url = currPlayerStatus.PlaybackContext.ExternalURLs["spotify"]
+	}
 	if err := clipboard.WriteAll(url); err != nil {
 		return err
 	}
